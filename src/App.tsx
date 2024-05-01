b886a37e771f011ca31b4d96e6d315b0bafe2dea
Namrah1 <namrah.siddiqui@compass-dx.com>
2024-05-01 19:13:06 +0500

    Charts Update

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ const Products = lazy(() => import("./pages/products"));
 const Customer = lazy(() => import("./pages/customer"));
 const Transaction = lazy(() => import("./pages/transaction"));
 
+const LineCharts = lazy(() => import("./pages/charts/lineCharts"));
+const PieCharts = lazy(() => import("./pages/charts/pieCharts"));
+const BarCharts = lazy(() => import("./pages/charts/barCharts"));
+
+const NewProduct = lazy(() => import("./pages/Management/newProduct"));
+const ProductManagement = lazy(() => import("./pages/Management/productManagement"));
+const TransactionManagement = lazy(() => import("./pages/Management/transactionManagement"));
 
 
 const App = () => {
@@ -21,15 +28,20 @@ const App = () => {
         <Route path="/admin/transaction" element={<Transaction />}></Route>
 
         {/* Charts */}
-        <Route path="/admin/chart/bar" element={<Dashboard />}></Route>
-        <Route path="/admin/chart/pie" element={<Customer/>}></Route>
-        <Route path="/admin/chart/line" element={<Products/>}></Route>
+        <Route path="/admin/chart/bar" element={<BarCharts/>}></Route>
+        <Route path="/admin/chart/pie" element={<PieCharts/>}></Route>
+        <Route path="/admin/chart/line" element={<LineCharts/>}></Route>
        
 
         {/* Apps */}
         <Route path="/admin/app/stopwatch" element={<Dashboard />}></Route>
         <Route path="/admin/app/coupon" element={<Customer/>}></Route>
         <Route path="/admin/app/toss" element={<Products/>}></Route>
+
+        {/*Management */}
+        <Route path="/admin/product/new" element={<NewProduct/>}/>
+        <Route path="/admin/product/:id" element={<ProductManagement/>}/>
+        <Route path="/admin/transaction/:id" element={<TransactionManagement/>}/>
       </Routes>
       </Suspense>
      
